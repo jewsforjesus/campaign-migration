621bffbce8efdfd00087fa445b0a06acde0701bd
Milder Lisondra <milder.lisondra@yahoo.com>
2021-02-10 19:14:50 -0800

    Update add-to-cm.php

--- add-to-cm.php
+++ add-to-cm.php
@@ -4,12 +4,19 @@ error_reporting(E_ERROR);
 require 'vendor/j4mie/idiorm/idiorm.php';
 require 'vendor/autoload.php';
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('CM-Import');
+$log_filename = 'logs/cm-import-'.time().'.log';
+$log->pushHandler(new StreamHandler(__DIR__.'/'.$log_filename, Logger::INFO));
+$log->info('Import Into Campaign Monitor', array('Start Time' => date('Y-m-d H:i:s')));
 
 $live_list_id = '4e7469f84cca66107bfc0c39542b6a11';
 $test_list_id = 'f382c03b099b30557e36175e4256f5ff';
 
 $env = 'dev';
-$select_limit = 1;
+$select_limit = 120;
 
 $db_host = '127.0.0.1';
 $db_name = 'ems_bbec';
@@ -20,12 +27,11 @@ ORM::configure('mysql:host='.$db_host.';dbname='.$db_name);
 ORM::configure('username', $db_user);
 ORM::configure('password', '');
 
-$results = ORM::for_table('subscribers_onl')->where(array('CMUpdated'=>'No'))->limit($select_limit)->find_array();
+$results = ORM::for_table('subscribers_uj_issues')->where(array('CMUpdated'=>'No'))->limit($select_limit)->find_array();
 
 /*
 * Get a limited number of email addreses from table
 * Retrieve details from Campaign Monitor
-* Iterate through the Opt-in – Believer  key
 */
 if($results){
 	foreach($results as $result){
@@ -33,21 +39,79 @@ if($results){
 
 		// check to see if email address/contact exists in Campaign Monitor
 		$exist_check = get_cm_details($EmailAddress);
-		//print_r($exist_check);
+
 		if(isset($exist_check['Code'])){
-			print 'Need to Add: ' . $EmailAddress . PHP_EOL;
-			add_to_cm($result);
-		}else{
-			print $EmailAddress . ' exists in CM ' . PHP_EOL; 
+			if($exist_check['Code'] == 203){
+				$add_result = add_to_cm(refine_data($result));
+				if($add_result == true){
+					print 'Contact Added: ' . $EmailAddress . PHP_EOL;
+				}
+			}else{ 
+				$log->info('Error received from Campaign Monitor', $exist_check);
+			}
+		}else{ 
+			print 'Exists : ' . $EmailAddress .  PHP_EOL; 
 		}
-/*		if(update_cm_details($EmailAddress) === true){
-			print $EmailAddress . ' updated ' . PHP_EOL;
-			$contact = ORM::for_table('subscribers_onl')->where("EmailAddress", $EmailAddress)->find_one();
-			//$person = ORM::for_table('person')->where('name', 'Fred Bloggs')->find_one();
-			$contact->set('CMUpdated','Yes');
-			$contact->save();
-		}*/
 	}
+}
+
+/*
+* add_to_cm
+* Add a new contact to list via Campaign Monitor API
+* @param array $data
+*/
+function add_to_cm($data){
+
+	global $env;
+	global $live_list_id;
+	global $test_list_id;
+	global $log;
+
+	if($env == 'dev' ){
+		$list_id = $test_list_id;
+	}else{
+		$list_id = $live_list_id;
+	}
+	$post_array['EmailAddress'] = $data['Email Address']; 
+	$post_array['ConsentToTrack'] = "Yes";
+	unset($data['ID']);
+	unset($data['Email Address']);
+	unset($data['']);
+
+	$post_array['CustomFields'] = $data;
+
+	$json_post = json_encode($post_array, JSON_PRETTY_PRINT);
+
+	$curl = curl_init();
+
+	curl_setopt_array($curl, array(
+	  CURLOPT_URL => 'https://api.createsend.com/api/v3.2/subscribers/' . $list_id . '.json',
+	  CURLOPT_RETURNTRANSFER => true,
+	  CURLOPT_ENCODING => '',
+	  CURLOPT_MAXREDIRS => 10,
+	  CURLOPT_TIMEOUT => 0,
+	  CURLOPT_FOLLOWLOCATION => true,
+	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	  CURLOPT_CUSTOMREQUEST => 'POST',
+	  CURLOPT_POSTFIELDS =>$json_post,
+	  CURLOPT_HTTPHEADER => array(
+	    'Content-Type: application/json',
+	    'Authorization: Basic eGFNN1U2dkNYeUpHcm1EdkozQmVLeFZDaVFiRGZiei9CSzFLWmRkaC92UVNNeXRVUTcxMjY2bU53ZjFtOGhYVXlXdnljL09meVd3dHhoZ0cwVGkvaWZGSm5qcTVxQmQyeFVJOFNJV2h0RXBuTURudXc5WHpldGF2Qzl1K0VmL09hUmNNU3MxeXNWNWNqUUExRXVXdVFRPT06'
+	  ),
+	));
+
+	$response = curl_exec($curl);
+	curl_close($curl);
+
+	$filtered_response = str_replace('"', '', $response);
+	if($filtered_response == $post_array['EmailAddress']){
+		return true;
+	}else{
+		$captured_response[] = $response;
+		$log->info('Campaign Monitor Add Response: ', $captured_response);
+		return false;
+	}
+
 }
 
 function get_cm_details($EmailAddress){
@@ -87,10 +151,13 @@ function get_cm_details($EmailAddress){
 }
 
 /*
-* add_to_cm
+* refine_data
+* 
 * @param array $data
 */
-function add_to_cm($data){
+function refine_data($data){
+	
+	ksort($data);
 
 	unset($data['City']);
 	unset($data['State']);
@@ -106,21 +173,30 @@ function add_to_cm($data){
 	unset($data['ExistsInMC']);
 	unset($data['CMUpdated']);
 
+
 	foreach($data as $key=>$value){
+
 		if($key != 'ID'){
 			// Get the proper CM version of the field name
-			//print 'Current key: ' . $key . PHP_EOL;
 			$new_key = get_field_mapping($key);
-			
 			$data[$new_key] = $data[$key];
 			unset($data[$key]);
-
 		}
 		
 	}
 
-	print_r($data);
-
+	$refined_data_array = array();
+	foreach($data as $key=>$value){
+		if(!empty($key)){
+			if($key !== 'Email Address' && $key !== 'ID'){
+				$refined_data_array[] = array("Key"=>$key,"Field Value"=>$value);
+			}else{
+				$refined_data_array[$key] = $value;
+			}
+		}
+	}
+ 	$refined_data_array[] = array("Key"=>'Opt-in – UJ/UG',"Value"=>'Monthly Issues articles');
+	return($refined_data_array);
 }
 
 /* 
@@ -130,7 +206,7 @@ function add_to_cm($data){
 *
 */
 function get_field_mapping($key){
-
+	//print $key .PHP_EOL;
 	$fields = array(
 		"EmailAddress"=>"Email Address",
 		"FirstName"=>"First Name",
